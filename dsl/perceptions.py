4aa1171137f7fd810c6820a66c534b7c3179492a
chenty <709384286@qq.com>
2023-07-25 13:57:37 +0800

    update: implement destination detection (QRcode)

--- dsl/perceptions.py
+++ dsl/perceptions.py
@@ -1,6 +1,7 @@
 import rospy
 from sensor_msgs.msg import Image
 import cv2
+from pyzbar.pyzbar import decode
 import requests
 
 import json
@@ -9,7 +10,7 @@ import os
 import traceback
 
 # from ..utils.convert import ImageConverter
-from convert import ImageConverter
+from utils.convert import ImageConverter
 from cv_bridge import CvBridge
 
 URL = "http://47.101.169.122:8762/vision/detect_traffic_light_color"
@@ -23,16 +24,17 @@ rospy.init_node("image_retriever", anonymous=True)
 COLOR = None
 SIZE = None
 SIZE_THRESHOLD = 3200
+DESTINATION_MARK = "destination"
 
 
-def __retieve_iamge(compression=None):
+def __retrieve_iamge(compression=None):
     rgb_Image = rospy.wait_for_message("/camera/rgb/image_raw", Image, timeout=1)
     # height, width, rgb_data = rgb_Image.height, rgb_Image.width, rgb_Image.data
+    img_name = uuid.uuid1().hex
     ## reorganize rgb img data
     cv_img = cv_bridge.imgmsg_to_cv2(rgb_Image, desired_encoding="passthrough") # RGB
 
     if compression is not None:
-        img_name = uuid.uuid1().hex
         cv_img = img_cvt.compress_image(cv_img, ratio=compression)
 
     cv_img = cv2.cvtColor(cv_img, cv2.COLOR_RGB2BGR)
@@ -57,8 +59,17 @@ def __delete_image(img_path):
     os.remove(img_path)
 
 
+def __decode_qrcode(img_path):
+    img = cv2.imread(img_path)
+    qr_decoded = decode(img)
+
+    if len(qr_decoded) == 0:
+        return None
+    return qr_decoded[0].data
+
+
 def scan():
-    img_path = __retieve_iamge(compression=0.5)
+    img_path = __retrieve_iamge(compression=0.5)
     rsp = __query_image(img_path)
     __delete_image(img_path)
 
@@ -84,10 +95,37 @@ def is_traffic_light_green():
 
 def is_safe_distance():
     return SIZE is not None and SIZE <= SIZE_THRESHOLD
-    
+
+
+def is_destination():
+    img_path = __retrieve_iamge(compression=None)
+    decoded = __decode_qrcode(img_path)
+    __delete_image(img_path)
+    return decoded == DESTINATION_MARK 
 
 
 if __name__ == "__main__":
+    '''
+    decode qrcode
+    '''
+    ## 0
+    # # img = cv2.imread("/home/agilex/sandbox_app/images/dest.png")
+    # img = cv2.imread("/home/agilex/sandbox_app/images/c6807b8e2aad11ee80f3845cf327d053.png")
+    # qr_decoded = decode(img)
+
+    # data = qr_decoded[0].data
+    # print(data == "destination")
+
+    ## 1
+    # img_path = __retrieve_iamge(compression=None)
+    # decoded = __decode_qrcode(img_path)
+    # print(decoded)
+
+
+
+    '''
+    test perception functions
+    '''
     while (True):
         op = raw_input("input operation:")
 
@@ -103,5 +141,7 @@ if __name__ == "__main__":
             print(is_safe_distance())
         elif op == "o":
             __output_variables()
+        elif op == "dest?":
+            print(is_destination())
         else:
             print("{}Invalid operation '{}'{}".format("\033[0;31;47m", op, "\033[0m"))
